82a566d0b0e5036e3164c3394330aa0efef9ac26
cvs2svn <admin@example.com>
1991-08-13 06:46:09 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.38.1'.

--- etc/mkband.scm
+++
@@ -1,8 +0,0 @@
-;;; -*- Scheme -*-
-;;; $Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/etc/mkband.scm,v 1.5 1990/12/12 03:07:58 cph Exp $
-;;; Input file to build standard bands.
-
-(set-working-directory-pathname! "../sf")
-(load "make")
-(disk-save "../lib/runtime.com")
-(%exit)
