90ed5acfbaa93c64e3e16ce8f97324109ed19225
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1994-03-30 21:17:49 +0000

    Snapshot of new compiler.

--- v7/src/compiler/machines/spectrum/dassm1.scm
+++ v7/src/compiler/machines/spectrum/dassm1.scm
@@ -1,8 +1,8 @@
 #| -*-Scheme-*-
 
-$Id: dassm1.scm,v 4.18 1993/07/01 00:15:44 gjr Exp $
+$Id: dassm1.scm,v 4.18.1.1 1994/03/30 21:17:49 gjr Exp $
 
-Copyright (c) 1988-1993 Massachusetts Institute of Technology
+Copyright (c) 1988-1994 Massachusetts Institute of Technology
 
 This material was developed by the Scheme project at the Massachusetts
 Institute of Technology, Department of Electrical Engineering and
@@ -91,6 +91,16 @@ MIT in each case. |#
 	(newline)
 	(newline)
 	(disassembler/write-compiled-code-block block info)))))
+
+(define (compiler:disassemble-memory start words)
+  (fluid-let ((disassembler/write-offsets? false)
+	      (disassembler/write-addresses? true)
+	      (disassembler/base-address start))
+    (newline)
+    (newline)
+    (disassembler/write-instruction-stream
+     #F
+     (disassembler/instructions/address start (+ start (* 4 words))))))
 
 (define (disassembler/write-compiled-code-block block info)
   (let ((symbol-table (and info (dbg-info/labels info))))
@@ -134,7 +144,7 @@ MIT in each case. |#
       (lambda (offset instruction)
 	(disassembler/write-instruction symbol-table
 					offset
-					(lambda () (display instruction)))))))
+					(lambda () (display-instruction offset instruction)))))))
 
 (define (disassembler/for-each-instruction instruction-stream procedure)
   (let loop ((instruction-stream instruction-stream))
@@ -157,6 +167,14 @@ MIT in each case. |#
 	       (loop (disassembler/write-linkage-section block
 							 symbol-table
 							 index)))
+	      ((object-type?
+		(let-syntax ((ucode-type
+			      (macro (name) (microcode-type name))))
+		  (ucode-type manifest-closure))
+		(system-vector-ref block index))
+	       (loop (disassembler/write-manifest-closure-pattern block
+								  symbol-table
+								  index)))
 	      (else
 	       (disassembler/write-instruction
 		symbol-table
@@ -226,18 +244,25 @@ MIT in each case. |#
 	  (write-caches
 	   0
 	   compiled-code-block/objects-per-variable-cache
-	  (lambda (block index)
-	    (disassembler/write-variable-cache "Reference" block index))))
+	   (lambda (block index)
+	     (disassembler/write-variable-cache "Reference" block index))))
 	 ((2)
 	  (write-caches
 	   0
 	   compiled-code-block/objects-per-variable-cache
-	  (lambda (block index)
-	    (disassembler/write-variable-cache "Assignment" block index))))
+	   (lambda (block index)
+	     (disassembler/write-variable-cache "Assignment" block index))))
+	 ((4)
+	  (disassembler/write-instruction
+	   symbol-table
+	   (compiled-code-block/index->offset (1+ index))
+	   (lambda ()
+	     (write-string "Closure linkage cache"))))
 	 (else
 	  (error "disassembler/write-linkage-section: Unknown section kind"
 		 kind)))
       (1+ (+ index length)))))
+
 
 (define-integrable (variable-cache-name cache)
   ((ucode-primitive primitive-object-ref 2) cache 1))
@@ -260,7 +285,40 @@ MIT in each case. |#
       (else
        (error "disassembler/write-procedure-cache: Unknown cache kind"
 	      (vector-ref result 0))))))
+
+(define closure-entry-size 4)
 
+(define (disassembler/write-manifest-closure-pattern block symbol-table index)
+  (let* ((descriptor    (integer-divide (system-vector-ref block (+ index 1))
+					#x10000))
+	 (offset        (integer-divide-remainder descriptor))
+	 (multiclosure? (= offset 0))
+	 (closures      (if multiclosure?
+			    (integer-divide-quotient descriptor)
+			    1))
+	 (pattern-len   (if multiclosure?
+			    (+ 1 (* closures closure-entry-size))
+			    closure-entry-size))
+	 (closure-len   (object-datum (system-vector-ref block index)))
+	 (free-vars     (- closure-len pattern-len)))
+    (disassembler/write-instruction
+     symbol-table
+     (compiled-code-block/index->offset index)
+     (lambda ()
+       (write-string "#[MANIFEST-CLOSURE-PATTERN ")
+       (write closure-len)
+       (if multiclosure?
+	   (begin (write-string " ")
+		  (write closures)
+		  (write-string "-closure")))
+       (write-string " with ")
+       (write free-vars)
+       (write-string " free variable")
+       (if (not (= free-vars 1))
+	   (write-string "s"))
+       (write-string "]")))
+    (+ index pattern-len 1)))    
+
 (define (disassembler/write-instruction symbol-table offset write-instruction)
   (if symbol-table
       (let ((label (dbg-labels/find-offset symbol-table offset)))
@@ -286,3 +344,172 @@ MIT in each case. |#
       (write-string "    "))
   (write-instruction)
   (newline))
+
+(let-syntax ((define-codes
+	       (macro (start . names)
+		 (define (loop names index assocs)
+		   (if (null? names)
+		       `((DEFINE CODE:COMPILER-XXX-ALIST ',assocs))
+		       (loop (cdr names) (1+ index)
+			     (cons (cons index (car names)) assocs))))
+		 `(BEGIN ,@(loop names start '())))))
+  ;; Copied from lapgen.scm
+  (define-codes #x012
+    primitive-apply primitive-lexpr-apply
+    apply error lexpr-apply link
+    interrupt-closure interrupt-dlink interrupt-procedure 
+    interrupt-continuation interrupt-ic-procedure
+    assignment-trap cache-reference-apply
+    reference-trap safe-reference-trap unassigned?-trap
+    -1+ &/ &= &> 1+ &< &- &* negative? &+ positive? zero?
+    access lookup safe-lookup unassigned? unbound?
+    set! define lookup-apply primitive-error
+    quotient remainder modulo
+    reflect-to-interface interrupt-continuation-2
+    compiled-code-bkpt compiled-closure-bkpt
+    new-interrupt-procedure))
+
+(let-syntax ((define-hooks
+	       (macro (start . names)
+		 (define (loop names index assocs)
+		   (if (null? names)
+		       `((DEFINE HOOK:COMPILER-XXX-ALIST ',assocs))
+		       (loop (cdr names) (+ 8 index)
+			     (cons (cons index (car names)) assocs))))
+		 `(BEGIN ,@(loop names start '())))))
+  ;; Copied from lapgen.scm
+  (define-hooks 100
+    store-closure-code
+    store-closure-entry			; newer version of store-closure-code.
+    multiply-fixnum
+    fixnum-quotient
+    fixnum-remainder
+    fixnum-lsh
+    &+
+    &-
+    &*
+    &/
+    &=
+    &<
+    &>
+    1+
+    -1+
+    zero?
+    positive?
+    negative?
+    shortcircuit-apply
+    shortcircuit-apply-1
+    shortcircuit-apply-2
+    shortcircuit-apply-3
+    shortcircuit-apply-4
+    shortcircuit-apply-5
+    shortcircuit-apply-6
+    shortcircuit-apply-7
+    shortcircuit-apply-8
+    stack-and-interrupt-check
+    invoke-primitive
+    vector-cons
+    string-allocate
+    floating-vector-cons
+    flonum-sin
+    flonum-cos
+    flonum-tan
+    flonum-asin
+    flonum-acos
+    flonum-atan
+    flonum-exp
+    flonum-log
+    flonum-truncate
+    flonum-ceiling
+    flonum-floor
+    flonum-atan2
+    compiled-code-bkpt
+    compiled-closure-bkpt
+    copy-closure-pattern
+    copy-multiclosure-pattern
+    closure-entry-bkpt-hook
+    interrupt-procedure/new
+    interrupt-continuation/new))
+
+(define display-instruction
+  (let ((prev-instruction '())
+	(prev-prev-instruction '()))
+    (lambda (offset instruction)
+
+      (define (unannotated) (display instruction))
+
+      (define (annotated)
+	(let ((s (with-output-to-string (lambda() (display instruction)))))
+	  (write-string s)
+	  (write-string (make-string (max 1 (- 40 (string-length s))) #\Space))
+	  (write-string ";")))
+
+      (define (annotate-with-name name)
+	(annotated)
+	(write-string " ")
+	(display name))
+
+      (define (annotate-with-target address)
+	(annotated)
+	(write-string " ")
+	(write-string (number->string address 16)))
+
+      (define (match? pat obj)
+	(or (eq? pat '?)
+	    (and (eq? pat '?n) (number? obj))
+	    (and (pair? pat) (pair? obj)
+		 (match? (car pat) (car obj))
+		 (match? (cdr pat) (cdr obj)))
+	    (equal? pat obj)))
+
+      (define (code?)
+	(match? '(ble ? (offset ? 4 3)) instruction))
+      (define (code-name)
+	(let ((id.name (assoc (second (third instruction))
+			      hook:compiler-xxx-alist)))
+	  (and id.name
+	       (cdr id.name))))
+
+      (define (hook?)
+	(and (or (equal? '(ble () (offset 0 4 3)) prev-instruction)
+		 (equal? '(ble () (offset 12 4 3)) prev-instruction))
+	     (match? '(ldi () ? 28) instruction)))
+      (define (hook-name)
+	(let ((id.name  (assoc (third instruction) code:compiler-xxx-alist)))
+	  (and id.name
+	       (cdr id.name))))
+    
+      (define (external-label?)
+	(match? '(external-label . ?) instruction))
+
+      (define (offset->address field adjustment)
+	(+ (+ offset disassembler/base-address) field adjustment))
+      (define (offset-targets)
+	(let ((res
+	       (map (lambda (@pco.n)
+		      (offset->address (second @pco.n) 8))
+		    (list-transform-positive instruction
+		      (lambda (part) (and (pair? part)
+					  (eq? (car part) '@pco)
+					  (not (equal? (cadr part) 0))))))))
+	  (if (null? res) #f res)))
+
+      (define (special-offset-target)
+	(cond ((and (match? '(bl () ? (@pco 0))      prev-instruction)
+		    (match? '(? ? (offset ?n 0 ?) ?) instruction)
+		    (eqv? (third prev-instruction) (fourth (third instruction))))	      
+	       (offset->address (second (third instruction)) (+ 8 -4 3)))
+	      ((match? '(uword () ?n) instruction)
+	       (offset->address (third instruction) 3))
+	      (else #f)))
+
+      (cond ((and (code?) (code-name)) => annotate-with-name)
+	    ((and (hook?) (hook-name)) => annotate-with-name)
+	    ((external-label?)  (unannotated))
+	    ((special-offset-target)   => annotate-with-target)
+	    ((offset-targets)          => (lambda (x)
+					    (annotate-with-target (car x))))
+	    (else  	     (unannotated)))
+
+      (set! prev-prev-instruction prev-instruction)
+      (set! prev-instruction instruction))))
