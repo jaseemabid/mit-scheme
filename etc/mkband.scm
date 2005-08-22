6a084d3ceadd252a109d3f715dbcd2892ccf4f62
cvs2svn <admin@example.com>
2005-08-22 01:15:09 +0000

    This commit was manufactured by cvs2svn to create branch 'pre-v15'.

--- etc/mkband.scm
+++
@@ -1,8 +0,0 @@
-;;; -*- Scheme -*-
-;;; $Id: mkband.scm,v 1.6 2000/03/21 17:32:59 cph Exp $
-;;; Input file to build standard bands.
-
-(set-working-directory-pathname! "../sf")
-(load "make")
-(disk-save "../lib/runtime.com")
-(%exit)
